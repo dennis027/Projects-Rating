048f564f4572cd90bb00a9db1c87627e7436a21e
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-20 11:49:43 +0300

    new rating model

--- projects/models.py
+++ projects/models.py
@@ -16,31 +16,39 @@ class Post(models.Model):
 
     @classmethod
     def search_by_title(cls,search_term):
-        post = cls.objects.filter(title__icontains=search_term)
-        return post
-class RateModel(models.Model):
-    pass
-    # TEN_REVIEWS= (
-    #     ('10', '10','10'),
-    #     ('9', '9','9'),
-    #     ('8', '8','8'),
-    #     ('7', '7','7'),
-    #     ('6', '6','6'),
-    #     ('5', '5','5'),
-    #     ('4', '4','4'),
-    #     ('3', '3','3'),
-    #     ('2', '2','2'),
-    #     ('1', '1','1'),
-    # )
+        posts = cls.objects.filter(title__icontains=search_term)
+        return posts
+class Rating(models.Model):
+    rating = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
 
-    # author = models.ForeignKey(User,on_delete=models.CASCADE)
-    # posts = models.ForeignKey(Post,on_delete = models.CASCADE)
-    # design = models.PositiveIntegerField(choices=TEN_REVIEWS,default=0)
-    # usability = models.PositiveIntegerField(choices=TEN_REVIEWS,default=0)
-    # content =models.PositiveIntegerField(choices=TEN_REVIEWS,default=0)
+    design = models.IntegerField(choices=rating, default=0, blank=True)
+    usability = models.IntegerField(choices=rating, blank=True)
+    content = models.IntegerField(choices=rating, blank=True)
+    score = models.FloatField(default=0, blank=True)
+    design_average = models.FloatField(default=0, blank=True)
+    usability_average = models.FloatField(default=0, blank=True)
+    content_average = models.FloatField(default=0, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='rater')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings', null=True)
 
-    # class Meta:
-    #     verbose_name_plural= 'Ratings'
+    def save_rating(self):
+        self.save()
 
-    # def __str__(self):
-    #     return self.str(self.post)   
+    @classmethod
+    def get_ratings(cls, id):
+        ratings = Rating.objects.filter(post_id=id).all()
+        return ratings
+
+    def __str__(self):
+        return f'{self.post} Rating'
